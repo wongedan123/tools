f2214f4d9a59e4fe6d1783381cab43deebcec4b2
Wongedan <47300852+wongedan123@users.noreply.github.com>
2019-03-13 03:36:00 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,18 @@
+clear
+echo ""
+echo "please wait deleting old tools and installing new tools :)"
+sleep 2
+ls
+sleep 1
+rm -rf tools
+git clone https://github.com/wongedan123/tools
+sleep 2
+clear
+echo ""
+echo "update finish :)"
+sleep 2
+clear
+cd tools
+echo " in tools wait 2 seccond"
+sleep 2
+python2 tool.py
